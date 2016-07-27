1587bf9d2b31403e2d8f90c19a0dfe877669d00b
Maya Zhecheva <maya.zhecheva@progress.com>
2016-07-27 14:46:05 +0300

    Add Button BindText

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,13 +5,15 @@ import { ExamplesListComponent } from "./examples-list.component";
 import { ButtonExamplesComponent } from "./button/button-examples.component";
 import { ButtonTextComponent } from "./button/text/text.component";
 import { ButtonTapEventComponent } from "./button/tap-event/tap-event.component";
+import { ButtonBindingTextComponent } from "./button/binding-text/binding-text.component";
 
 export const routes: RouterConfig = [
   { path: "", component: ExamplesListComponent, data: { title: "Examples List" } },
 
   { path: "buttonExamplesComponent", component: ButtonExamplesComponent, data: { title: "Button" } },
   { path: "buttonTextExampleComponent", component: ButtonTextComponent, data: { title: "Text" } },
-  { path: "buttonTapEventExampleComponent", component: ButtonTapEventComponent, data: { title: "Tap Event" } }
+  { path: "buttonTapEventExampleComponent", component: ButtonTapEventComponent, data: { title: "Tap Event" } },
+  { path: "buttonBindingTextExampleComponent", component: ButtonBindingTextComponent, data: { title: "Binding Text" } }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ app/button/binding-text/binding-text.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { COMMON_DIRECTIVES } from '../../directives';
+import { EventData } from "data/observable";
+import { Page } from "ui/page";
+import observable = require("data/observable");
+
+@Component({
+    selector: 'button-component',
+    directives: [COMMON_DIRECTIVES],
+    templateUrl: 'button/binding-text/binding-text.component.html'
+})
+
+export class ButtonBindingTextComponent {      
+   model : observable.Observable;
+
+   constructor() {
+    this.model = new observable.Observable();
+    this.model.set("content", "Button Text");
+  }
+}
