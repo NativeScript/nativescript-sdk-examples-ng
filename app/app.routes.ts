9adf1d0a93d457d41497f73daed7289329ef2e11
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-07-28 17:48:02 +0300

    ListView creation and customization exmaples added

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -13,6 +13,10 @@ import { ProgressExamplesComponent } from "./progress/progress-examples.componen
 import { CreatingProgressComponent } from "./progress/creating-progress/creating-progress.component";
 import { SettingProgressComponent } from "./progress/setting-progress/setting-progress.component";
 
+import { ListViewExamplesComponent } from "./listview/listview-examples.component";
+import { CreatingListViewComponent } from "./listview/creating-listview/creating-listview.component";
+import { CustomizingListViewComponent } from "./listview/customizing-listview/customizing-listview.component";
+
 export const routes: RouterConfig = [
   { path: "", component: ExamplesListComponent, data: { title: "Examples List" } },
 
@@ -20,18 +24,22 @@ export const routes: RouterConfig = [
   { path: "buttonTextExampleComponent", component: ButtonTextComponent, data: { title: "Text" } },
   { path: "buttonTapEventExampleComponent", component: ButtonTapEventComponent, data: { title: "Tap Event" } },
 
-  { path: "ngForExampleComponent", component: NgForExamplesComponent, data: { title: "*ngFor" } },
-  { path: "ngForRepeatItemsComponent", component: NgForRepeatItemsComponent, data: { title: "*ngFor Repeat Items" } },
+  { path: "listViewExamplesComponent", component: ListViewExamplesComponent, data: { title: "ListView" } },
+  { path: "creatingListViewExampleComponent", component: CreatingListViewComponent, data: { title: "Creating ListView" } },
+  { path: "customizingListViewExampleComponent", component: CustomizingListViewComponent, data: { title: "Customizing ListView" } },
+  // { path: "usingItemTemplateExampleComponent", component: ProgressExamplesComponent, data: { title: "Using ListView Item Template" } },
+  // { path: "usingAsyncPipeExampleComponent", component: ProgressExamplesComponent, data: { title: "Using Async Pipe" } },
 
   { path: "progressExamplesComponent", component: ProgressExamplesComponent, data: { title: "Progress" } },
   { path: "creatingProgressExampleComponent", component: CreatingProgressComponent, data: { title: "Creating Progress" } },
-  { path: "settingProgressExampleComponent", component: SettingProgressComponent, data: { title: "Setting up Progress" } }    
+  { path: "settingProgressExampleComponent", component: SettingProgressComponent, data: { title: "Setting up Progress" } },   
+
+  { path: "ngForExampleComponent", component: NgForExamplesComponent, data: { title: "*ngFor" } },
+  { path: "ngForRepeatItemsComponent", component: NgForRepeatItemsComponent, data: { title: "*ngFor Repeat Items" } }
+
 ];
 
 export const APP_ROUTER_PROVIDERS = [
   nsProvideRouter(routes, { })
 ];
-
-
- 
-
+  
---
+++ app/listview/creating-listview/article.md
@@ -0,0 +1,8 @@
+HTML
+<snippet id='listview-create-html'/>
+
+TypeScript
+<snippet id='listview-create-code'/>
+
+CSS
+<snippet id='listview-create-css'/>
