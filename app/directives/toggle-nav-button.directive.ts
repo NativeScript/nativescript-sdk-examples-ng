0ec38d65b25d63e0b505f9c55beb6460bc26ee03
Maya Zhecheva <maya.zhecheva@progress.com>
2016-08-16 13:19:20 +0300

    ux-update-navigation-button

--- app/directives/toggle-nav-button.directive.ts
+++ app/directives/toggle-nav-button.directive.ts
@@ -1,14 +1,43 @@
-import { Directive } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Directive } from "@angular/core";
+import { EventData } from "data/observable";
+import { NavigationButton } from "ui/action-bar";
 import { Page } from "ui/page";
+import { RouterExtensions } from 'nativescript-angular/router';
+import app = require("application");
 
 @Directive({
     selector: "[toggleNavButton]"
 })
 
 export class ToggleNavButtonDirective {
-     constructor(route: ActivatedRoute, page:Page) {
-        page.actionBar.navigationButton.visibility = "visible";
-        console.log("page.actionBar.navigationButton.visibility: " + page.actionBar.navigationButton.visibility);
+    constructor(route: ActivatedRoute, page: Page, private routerExtensions: RouterExtensions) {
+        let navigationButton = this.createNavigationButton();
+        page.actionBar.navigationButton = navigationButton;
+        this.toggleNavigationButtonVisibility(page.actionBar.navigationButton);
+    }
+
+    createNavigationButton(): NavigationButton {
+        let navigationButton = new NavigationButton();
+        navigationButton.visibility = "visible";
+
+        if (app.android) {
+            navigationButton.android.systemIcon = "ic_menu_back";
+
+            navigationButton.on("tap", (args: EventData) => {
+                this.routerExtensions.backToPreviousPage();
+            });
+        }
+        else if (app.ios) {
+            navigationButton.text = "";
+        }
+
+        return navigationButton;
+    }
+
+    toggleNavigationButtonVisibility(button: NavigationButton) {
+        if (button.actionBar.title === "Examples") {
+            button.visibility = "collapsed";
+        }
     }
 }
