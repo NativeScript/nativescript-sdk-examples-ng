41779eda6643acec0061cc4a1a985150daeea9db
Maya Zhecheva <maya.zhecheva@progress.com>
2016-09-01 18:47:21 +0300

    ux-layouts-updates

--- app/layouts/absolute-layout/absolute-layout.component.ts
+++ app/layouts/absolute-layout/absolute-layout.component.ts
@@ -2,7 +2,7 @@ import { AbsoluteLayout} from "ui/layouts/absolute-layout"
 import { Color } from "color";
 import { Component, OnInit } from "@angular/core";
 import { COMMON_DIRECTIVES } from '../../directives';
-import { Label } from "ui/label"
+import { Button } from "ui/button"
 import { Page } from "ui/page";
 
 @Component({
@@ -17,31 +17,30 @@ export class AbsoluteLayoutComponent implements OnInit {
 
     ngOnInit() {
         let absoluteLayout = this.page.getViewById<AbsoluteLayout>("absoluteLayout");
-        absoluteLayout.style.backgroundColor = new Color("#F1F0FF");
 
         // >> absolute-layout-code
-        var firstLabel = new Label();
+        var firstButton = new Button();
 
-        AbsoluteLayout.setLeft(firstLabel, 175);
-        AbsoluteLayout.setTop(firstLabel, 50);
-        firstLabel.width = 170;
-        firstLabel.height = 100;
+        AbsoluteLayout.setLeft(firstButton, 175);
+        AbsoluteLayout.setTop(firstButton, 50);
+        firstButton.width = 170;
+        firstButton.height = 100;
 
-        absoluteLayout.addChild(firstLabel);
+        absoluteLayout.addChild(firstButton);
         // << absolute-layout-code
 
-        firstLabel.text = "Left: 175, Top: 50";
-        firstLabel.style.backgroundColor = new Color("#CCFFFF");
+        firstButton.text = "Left: 175, Top: 50";
+        firstButton.style.backgroundColor = new Color("#CCFFFF");
 
-        var secondLabel = new Label();
+        var secondButton = new Button();
 
         // In absolute layout place of an UI element is determined by 4 parameters : left, top, width and height.
-        AbsoluteLayout.setLeft(secondLabel, 70);
-        AbsoluteLayout.setTop(secondLabel, 200);
-        secondLabel.width = 200;
-        secondLabel.height = 30;
-        secondLabel.text = "Left: 70, Top: 200";
-        absoluteLayout.addChild(secondLabel);
-        secondLabel.style.backgroundColor = new Color("#8C489F");
+        AbsoluteLayout.setLeft(secondButton, 70);
+        AbsoluteLayout.setTop(secondButton, 200);
+        secondButton.width = 230;
+        secondButton.height = 60;
+        secondButton.text = "Left: 70, Top: 200";
+        absoluteLayout.addChild(secondButton);
+        secondButton.style.backgroundColor = new Color("#8C489F");
     }
 }
