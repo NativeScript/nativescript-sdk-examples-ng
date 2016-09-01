5650c0b45beb45ca171bfb825fc7600274c311b2
Maya Zhecheva <maya.zhecheva@progress.com>
2016-09-01 16:57:25 +0300

    Removed unused import

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
-import { APP_ROUTER_PROVIDERS, routes, routableComponents } from "./app.routes";
+import { routes, routableComponents } from "./app.routes";
 import { AppComponent } from "./app.component";
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
