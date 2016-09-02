7438c7bd3c43e2c3329fde1472dc0c70d414a901
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2016-09-02 13:33:29 +0300

    edit datepicker timepicker articles

--- app/date-picker/configure-date-picker/configure-date-picker.component.ts
+++ app/date-picker/configure-date-picker/configure-date-picker.component.ts
@@ -1,21 +1,25 @@
+// >> date-picker-configure-code
 import { Component } from "@angular/core";
+// >> (hide)
 import { COMMON_DIRECTIVES } from '../../directives';
+// << (hide)
 import { DatePicker } from "ui/date-picker";
 
 @Component({
     selector: 'date-picker-component',
+    // >> (hide)
     directives: [COMMON_DIRECTIVES],
+    // << (hide)
     templateUrl: 'date-picker/configure-date-picker/configure-date-picker.component.html'
 })
 
 export class ConfigureDatePickerComponent {
     configure(datePicker: DatePicker) {
-        // >> date-picker-configure-code
         datePicker.year = 1980;
         datePicker.month = 2;
         datePicker.day = 9;
         datePicker.minDate = new Date(1975, 0, 29);
         datePicker.maxDate = new Date(2045, 4, 12);
-        // << date-picker-configure-code
     }
 }
+// << date-picker-configure-code
--- app/time-picker/configure-time-picker/configure-time-picker.component.ts
+++ app/time-picker/configure-time-picker/configure-time-picker.component.ts
@@ -1,19 +1,23 @@
+// >> time-picker-configure-code
 import { Component } from "@angular/core";
+// >> (hide)
 import { COMMON_DIRECTIVES } from '../../directives';
+// << (hide)
 import { TimePicker } from "ui/time-picker";
 
 @Component({
     selector: 'time-picker-component',
+    // >> (hide)
     directives: [COMMON_DIRECTIVES],
+    // << (hide)
     templateUrl: 'time-picker/configure-time-picker/configure-time-picker.component.html'
 })
 
 export class ConfigureTimePickerComponent {
 
     configure(timePicker: TimePicker) {
-        // >> time-picker-configure-code
         timePicker.hour = 9;
-        timePicker.minute = 25;
-        // << time-picker-configure-code
+        timePicker.minute = 25;  
     }
 }
+// << time-picker-configure-code
