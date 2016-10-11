103eb5b6a7126754fe5edfc0152c8efa1a2580d0
Maya Zhecheva <maya.zhecheva@progress.com>
2016-10-11 13:08:07 +0300

    updated build script

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,7 @@ var rimraf = require("rimraf");
 var pjson = require('../package.json');
 var child_process = require('child_process');
 var targz = require('tar.gz');
-  
+
 // `i-am-article` becomes `I Am Article`
 function prettify(str) {
     return str.split('-').map(function capitalize(part) {
@@ -22,38 +22,56 @@ function compareFiles (leftFile, rightFile) {
     return l.localeCompare(r, "en-US");
 }
 
-function build(){
+function build() {
     // Re-create the `dist` dir
     var cwd = process.cwd();
     var distDir = path.join(cwd, "dist");
     rimraf.sync(distDir);
     fs.mkdirSync(distDir);
-  
+
     var articlesDir = path.join(distDir, "sdk-examples");
     fs.mkdirSync(articlesDir);
-      
-    var imgDir = path.join(articlesDir, "img");
-    fs.mkdirSync(imgDir);  
 
     var appDir = path.join(cwd, "app");
-   
+
     var mainOverview = path.join(appDir, "global-overview.md");
     fs.copySync(mainOverview, path.join(articlesDir, "overview.md"));
 
-    var subDirs = fs.readdirSync(appDir).filter(function(file) {
-        return fs.statSync(path.join(appDir, file)).isDirectory();
+    var jenkinsPosition = 1;
+
+    // Gather all main subfolders - ui
+    var subDirs = fs.readdirSync(appDir).filter(function (file) {
+        var filePath = path.join(appDir, file);
+        var dir = fs.statSync(filePath);
+
+        return dir.isDirectory() && path.parse(filePath).name == "ui";
     });
 
-    subDirs.forEach(function(subDir) {
+    subDirs.forEach(function (subDir) {
         var currentDir = path.join(articlesDir, subDir);
         fs.mkdirSync(currentDir);
 
         var subDirPath = path.join(appDir, subDir);
-        // Gather all component overviews
-        var components = glob.sync(subDirPath + "/**/overview.md").sort(compareFiles);  
 
-        var jenkinsPosition = 1;
-        components.forEach(function(overview){        
+        // Gather all component overviews in the subdirs - ui
+        var components = glob.sync(subDirPath + "/**/overview.md").sort(compareFiles);
+        getComponents(cwd, components, currentDir, jenkinsPosition);
+    });
+
+    // Gather all component overviews in the main folder - app
+    var components = glob.sync(appDir + "/**/overview.md").filter(function (file) {
+        return path.parse(file).dir.indexOf("ui") === -1;
+    }).sort(compareFiles);
+    getComponents(cwd, components, articlesDir, jenkinsPosition);
+}
+
+// Gather all component overviews
+function getComponents(cwd, components, currentDir, jenkinsPosition) {
+      var imgDir = path.join(currentDir, "img");
+      fs.mkdirSync(imgDir);
+      console.log("SHOSHO imgDir " + imgDir);
+
+      components.forEach(function (overview) {
             var componentDirName = path.dirname(overview);
             var componentHeader = path.basename(componentDirName);
 
@@ -63,70 +81,69 @@ function build(){
             var componentPrettyHeader = prettify(componentHeader);
 
             // Jenkins Header
-            fs.appendFileSync(componentArticleFile, "---\n", {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, "title: " + componentPrettyHeader + "\n", {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, "description: " + componentPrettyHeader + " SDK Examples" + "\n", {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, "position: " + jenkinsPosition++ + "\n", {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, "slug: " + componentHeader + "\n", {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, "---\n\n", {encoding: 'utf8'});
-            
-            // Component Markdown Header
-            fs.appendFileSync(componentArticleFile, "# " + componentPrettyHeader + "\n\n", {encoding: 'utf8'});
-            
-            // Component Overview
-            var overviewContents = fs.readFileSync(overview, {encoding: 'utf8'});
-            fs.appendFileSync(componentArticleFile, overviewContents + "\n\n", {encoding: 'utf8'});
+            fs.appendFileSync(componentArticleFile, "---\n",  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, "title: " + componentPrettyHeader + "\n",  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, "description: " + componentPrettyHeader + " SDK Examples" + "\n",  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, "position: " + jenkinsPosition +  +  + "\n",  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, "slug: " + componentHeader + "\n",  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, "---\n\n",  {encoding:'utf8'});
 
+            // Component Markdown Header
+            fs.appendFileSync(componentArticleFile, "# " + componentPrettyHeader + "\n\n",  {encoding:'utf8'});
+
+            // Component Overview
+            var overviewContents = fs.readFileSync(overview,  {encoding:'utf8'});
+            fs.appendFileSync(componentArticleFile, overviewContents + "\n\n",  {encoding:'utf8'});
+console.log("SHOSHO componentDirName " + componentDirName);
             // Component Images
             let componentImage = path.join(componentDirName, "image.png");
             if (fs.existsSync(componentImage)) {
-                let newImageFileName = componentHeader + "-" +  "image.png";
-            
+                let newImageFileName = componentHeader + "-" + "image.png";
+console.log("SHOSHO newImageFileName " + newImageFileName);
                 fs.copySync(componentImage, path.join(imgDir, newImageFileName));
 
-                fs.appendFileSync(componentArticleFile, "![Image](../img/" + newImageFileName + " \"Image\")\n\n", { encoding: 'utf8' });
+                fs.appendFileSync(componentArticleFile, "![Image](img/" + newImageFileName + " \"Image\")\n\n",  {encoding:'utf8'});
             }
 
             var articles = glob.sync(componentDirName + "/**/article.md").sort(compareFiles);
-            
+
             // Append each example to the big article file.
-            articles.forEach(function(article) {
+            articles.forEach(function (article) {
                 var articleDirName = path.dirname(article);
                 var articleHeader = path.basename(articleDirName);
 
                 // Header
                 var prettyArticleHeader = prettify(articleHeader);
-                fs.appendFileSync(componentArticleFile, "## " + prettyArticleHeader + "\n\n", {encoding: 'utf8'});
+                fs.appendFileSync(componentArticleFile, "## " + prettyArticleHeader + "\n\n",  {encoding:'utf8'});
 
                 // Content
-                var articleContents = fs.readFileSync(article, {encoding: 'utf8'});
-                fs.appendFileSync(componentArticleFile, articleContents + "\n\n", {encoding: 'utf8'});
-                
+                var articleContents = fs.readFileSync(article,  {encoding:'utf8'});
+                fs.appendFileSync(componentArticleFile, articleContents + "\n\n",  {encoding:'utf8'});
+
                 // Article Images
                 let articleImage = path.join(articleDirName, "image.png");
-                
-                if (fs.existsSync(articleImage)){
+
+                if (fs.existsSync(articleImage)) {
                     let newArticleImageFileName = componentHeader + "-" + articleHeader + "-image.png";
                     let joined = path.join(imgDir, newArticleImageFileName);
-                    fs.copySync(articleImage, joined); 
+                    fs.copySync(articleImage, joined);
 
-                    fs.appendFileSync(componentArticleFile, "![Image](img/"+newArticleImageFileName+" \"Image\")\n\n", {encoding: 'utf8'});
+                    fs.appendFileSync(componentArticleFile, "![Image](img/" + newArticleImageFileName + " \"Image\")\n\n",  {encoding:'utf8'});
                 }
 
                 // Links
                 var githubDirUrl = pjson.homepage + "/edit/master/" + path.relative(cwd, articleDirName).replace(/\\/g, "/");
 
                 var linkToDocument = "[Improve this document](" + githubDirUrl + "/" + path.basename(article) + ")"
-                fs.appendFileSync(componentArticleFile, linkToDocument + "\n\n", {encoding: 'utf8'});
-                
+                fs.appendFileSync(componentArticleFile, linkToDocument + "\n\n",  {encoding:'utf8'});
+
                 var linkToSource = "[Demo Source](" + githubDirUrl + ")"
-                fs.appendFileSync(componentArticleFile, linkToSource + "\n\n", {encoding: 'utf8'});
+                fs.appendFileSync(componentArticleFile, linkToSource + "\n\n",  {encoding:'utf8'});
 
                 // Horizontal Line
-                fs.appendFileSync(componentArticleFile, "---\n\n", {encoding: 'utf8'});
+                fs.appendFileSync(componentArticleFile, "---\n\n",  {encoding:'utf8'});
             });
         });
-    });
 }
 
 build();
