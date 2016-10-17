50da085653a05d616475c6fb739c53f311669207
vchimev <Vasil.Chimev@telerik.com>
2016-10-17 13:07:40 +0300

    Update Trace Examples

--- app/trace/trace-examples.component.ts
+++ app/trace/trace-examples.component.ts
@@ -2,8 +2,8 @@ import { Component, ChangeDetectionStrategy }  from "@angular/core";
 import { Link } from "./../link";
 
 var menuLinks = [
-    new Link("Trace Specific categories Example", "/traceSpecificCategoriesExampleComponent"),
-    new Link("Writing a Custom TraceWriter Example", "/customTraceWriterExampleComponent")
+    new Link("Trace Specific categories", "/traceSpecificCategoriesExampleComponent"),
+    new Link("Writing a Custom TraceWriter", "/customTraceWriterExampleComponent")
 ];
 
 @Component({
