24e947d72381b465616700b6cfb3c2dd001ee1de
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2016-10-19 18:37:48 +0300

    edit style ui examples

--- app/ui/text-view/basic-text-view/basic-text-view.component.ts
+++ app/ui/text-view/basic-text-view/basic-text-view.component.ts
@@ -1,5 +1,7 @@
 // >> textview-edit-disable-code
 import { Component } from "@angular/core";
+import {TextView} from "ui/text-view"
+import {isAndroid} from "platform"
 
 @Component({
     selector: 'basic-text-view-component',
@@ -13,18 +15,28 @@ export class BasicTextViewComponent {
 
     public editState = true;
     public tvtext = "";
+    public buttonText = "Disable editting of TextView";
 
     disableTextView(){
         if(this.editState){
             this.editState = false;
+            this.buttonText = "Enable editting of TextView";
         }
         else{
             this.editState = true;
+            this.buttonText = "Disable editting of TextView";
         }
     }
 
     showText(){
         alert("Text: "+this.tvtext);
+    }
+
+    submit(args){
+        var textview:TextView = <TextView> args.object;
+        if(isAndroid){
+            textview.android.clearFocus();
+        }
     }
     
 }
