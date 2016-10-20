0a37d9c8b356a86dc6556c0ef11283a1a9809d9f
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-10-20 16:56:29 +0300

    refactor

--- app/ui-extended/listview/single-line-grouped/grouped-single-line.component.ts
+++ app/ui-extended/listview/single-line-grouped/grouped-single-line.component.ts
@@ -15,26 +15,26 @@ class GroupFooter{
 
 var mockedDataArray = [
     new GroupTitle("Asia"),
-    new Country("Japan", "~/ui-extended/listview/single-line/images/flags/jp.png", "Asia"),
-    new Country("China", "~/ui-extended/listview/single-line/images/flags/cn.png", "Asia"),
+    new Country("Japan", "~/ui-extended/listview/images/flags/jp.png", "Asia"),
+    new Country("China", "~/ui-extended/listview/images/flags/cn.png", "Asia"),
 
     new GroupTitle("Europe"),
-    new Country("Belgium", "~/ui-extended/listview/single-line/images/flags/be.png", "Europe"),
-    new Country("Bulgaria", "~/ui-extended/listview/single-line/images/flags/bg.png", "Europe"),
-    new Country("Switzerland", "~/ui-extended/listview/single-line/images/flags/ch.png", "Europe"),
-    new Country("Czech Republic", "~/ui-extended/listview/single-line/images/flags/cz.png", "Europe"),
-    new Country("Germany", "~/ui-extended/listview/single-line/images/flags/de.png", "Europe"),
-    new Country("Spain", "~/ui-extended/listview/single-line/images/flags/es.png", "Europe"),
-    new Country("Croatia", "~/ui-extended/listview/single-line/images/flags/hr.png", "Europe"),
-    new Country("Hungary", "~/ui-extended/listview/single-line/images/flags/hu.png", "Europe"),
-    new Country("Italy", "~/ui-extended/listview/single-line/images/flags/it.png", "Europe"),
-    new Country("Romania", "~/ui-extended/listview/single-line/images/flags/ro.png", "Europe"),
-    new Country("Russia", "~/ui-extended/listview/single-line/images/flags/ru.png", "Europe"),
+    new Country("Belgium", "~/ui-extended/listview/images/flags/be.png", "Europe"),
+    new Country("Bulgaria", "~/ui-extended/listview/images/flags/bg.png", "Europe"),
+    new Country("Switzerland", "~/ui-extended/listview/images/flags/ch.png", "Europe"),
+    new Country("Czech Republic", "~/ui-extended/listview/images/flags/cz.png", "Europe"),
+    new Country("Germany", "~/ui-extended/listview/images/flags/de.png", "Europe"),
+    new Country("Spain", "~/ui-extended/listview/images/flags/es.png", "Europe"),
+    new Country("Croatia", "~/ui-extended/listview/images/flags/hr.png", "Europe"),
+    new Country("Hungary", "~/ui-extended/listview/images/flags/hu.png", "Europe"),
+    new Country("Italy", "~/ui-extended/listview/images/flags/it.png", "Europe"),
+    new Country("Romania", "~/ui-extended/listview/images/flags/ro.png", "Europe"),
+    new Country("Russia", "~/ui-extended/listview/images/flags/ru.png", "Europe"),
 
     new GroupTitle("North America"),
-    new Country("Canada", "~/ui-extended/listview/single-line/images/flags/ca.png", "North America"),
-    new Country("United States", "~/ui-extended/listview/single-line/images/flags/us.png", "North America"),
-    new Country("Jamaica", "~/ui-extended/listview/single-line/images/flags/jm.png", "North America"),
+    new Country("Canada", "~/ui-extended/listview/images/flags/ca.png", "North America"),
+    new Country("United States", "~/ui-extended/listview/images/flags/us.png", "North America"),
+    new Country("Jamaica", "~/ui-extended/listview/images/flags/jm.png", "North America"),
 
     new GroupFooter("all countries loaded!")
 ]
--- app/ui-extended/listview/single-line/single-line-listview.component.ts
+++ app/ui-extended/listview/single-line/single-line-listview.component.ts
@@ -1,28 +1,29 @@
 import { Component, ChangeDetectionStrategy, OnInit, Input}  from "@angular/core";
+import { ItemEventData } from "ui/list-view";
 
 class Country {
     constructor(public name: string, public imageSrc: string, public continent: string) { }
 }
 
 var mockedDataArray = [
-    new Country("Australia", "~/ui-extended/listview/single-line/images/flags/au.png", "Australia"),
-    new Country("Belgium", "~/ui-extended/listview/single-line/images/flags/be.png", "Europe"),
-    new Country("Bulgaria", "~/ui-extended/listview/single-line/images/flags/bg.png", "Europe"),
-    new Country("Canada", "~/ui-extended/listview/single-line/images/flags/ca.png", "North America"),
-    new Country("Switzerland", "~/ui-extended/listview/single-line/images/flags/ch.png", "Europe"),
-    new Country("China", "~/ui-extended/listview/single-line/images/flags/cn.png", "Asia"),
-    new Country("Czech Republic", "~/ui-extended/listview/single-line/images/flags/cz.png", "Europe"),
-    new Country("Germany", "~/ui-extended/listview/single-line/images/flags/de.png", "Europe"),
-    new Country("Spain", "~/ui-extended/listview/single-line/images/flags/es.png", "Europe"),
-    new Country("Ethiopia", "~/ui-extended/listview/single-line/images/flags/et.png", "Africa"),
-    new Country("Croatia", "~/ui-extended/listview/single-line/images/flags/hr.png", "Europe"),
-    new Country("Hungary", "~/ui-extended/listview/single-line/images/flags/hu.png", "Europe"),
-    new Country("Italy", "~/ui-extended/listview/single-line/images/flags/it.png", "Europe"),
-    new Country("Jamaica", "~/ui-extended/listview/single-line/images/flags/jm.png", "North America"),
-    new Country("Japan", "~/ui-extended/listview/single-line/images/flags/jp.png", "Asia"),
-    new Country("Romania", "~/ui-extended/listview/single-line/images/flags/ro.png", "Europe"),
-    new Country("Russia", "~/ui-extended/listview/single-line/images/flags/ru.png", "Europe"),
-    new Country("United States", "~/ui-extended/listview/single-line/images/flags/us.png", "North America"),
+    new Country("Australia", "~/ui-extended/listview/images/flags/au.png", "Australia"),
+    new Country("Belgium", "~/ui-extended/listview/images/flags/be.png", "Europe"),
+    new Country("Bulgaria", "~/ui-extended/listview/images/flags/bg.png", "Europe"),
+    new Country("Canada", "~/ui-extended/listview/images/flags/ca.png", "North America"),
+    new Country("Switzerland", "~/ui-extended/listview/images/flags/ch.png", "Europe"),
+    new Country("China", "~/ui-extended/listview/images/flags/cn.png", "Asia"),
+    new Country("Czech Republic", "~/ui-extended/listview/images/flags/cz.png", "Europe"),
+    new Country("Germany", "~/ui-extended/listview/images/flags/de.png", "Europe"),
+    new Country("Spain", "~/ui-extended/listview/images/flags/es.png", "Europe"),
+    new Country("Ethiopia", "~/ui-extended/listview/images/flags/et.png", "Africa"),
+    new Country("Croatia", "~/ui-extended/listview/images/flags/hr.png", "Europe"),
+    new Country("Hungary", "~/ui-extended/listview/images/flags/hu.png", "Europe"),
+    new Country("Italy", "~/ui-extended/listview/images/flags/it.png", "Europe"),
+    new Country("Jamaica", "~/ui-extended/listview/images/flags/jm.png", "North America"),
+    new Country("Japan", "~/ui-extended/listview/images/flags/jp.png", "Asia"),
+    new Country("Romania", "~/ui-extended/listview/images/flags/ro.png", "Europe"),
+    new Country("Russia", "~/ui-extended/listview/images/flags/ru.png", "Europe"),
+    new Country("United States", "~/ui-extended/listview/images/flags/us.png", "North America"),
 ]
 
 // >> ext-listview-basic-code
@@ -40,15 +41,15 @@ export class SingleLineListViewExampleComponent implements OnInit {
         }
     }
 
-    onItemTapFirstList(args) {
+    onItemTapFirstList(args: ItemEventData) {
         console.log(args.index);
     }
 
-    onItemTapSecondList(args) {
+    onItemTapSecondList(args: ItemEventData) {
         console.log(args.index);
     }
 
-    onItemTapThirdList(args) {
+    onItemTapThirdList(args: ItemEventData) {
         console.log(args.index);
     }   
 }
