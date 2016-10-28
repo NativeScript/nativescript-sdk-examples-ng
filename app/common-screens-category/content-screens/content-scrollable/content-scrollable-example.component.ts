7bed53b2f639a2c3caae78daf541238229b2f828
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-10-28 16:50:01 +0300

    better content page example

--- app/common-screens-category/content-screens/content-scrollable/content-scrollable-example.component.ts
+++ app/common-screens-category/content-screens/content-scrollable/content-scrollable-example.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { ScrollEventData } from "ui/scroll-view";
 import { Page } from "ui/page";
 import { Image } from "ui/image";
+import { FlexboxLayout } from "ui/layouts/flexbox-layout";
 
 // >> content-scrollable-page-code
 @Component({
@@ -13,7 +14,10 @@ export class ContentScrollablePageExampleComponent implements OnInit {
     public desc: string;
     public imageSrc: string;
     public image: Image;
+    public flex: FlexboxLayout;
+
     @ViewChild("img") img: ElementRef;
+    @ViewChild("content") content: ElementRef;
 
     constructor(public page: Page) {
         this.page.backgroundSpanUnderStatusBar = true;
@@ -24,14 +28,19 @@ export class ContentScrollablePageExampleComponent implements OnInit {
         this.title = "The Hydrogen Clouds of M33";
         this.desc = "Gorgeous spiral galaxy M33 seems to have more than its fair share of glowing hydrogen gas. A prominent member of the local group of galaxies, M33 is also known as the Triangulum Galaxy and lies about 3 million light-years distant. The galaxy's inner 30,000 light-years or so are shown in this telescopic portrait that enhances its reddish ionized hydrogen clouds or HII regions. Sprawling along loose spiral arms that wind toward the core, M33's giant HII regions are some of the largest known stellar nurseries, sites of the formation of short-lived but very massive stars. Intense ultraviolet radiation from the luminous, massive stars ionizes the surrounding hydrogen gas and ultimately produces the characteristic red glow. To enhance this image, broadband data was used to produce a color view of the galaxy and combined with narrowband data recorded through a hydrogen-alpha filter. That filter transmits the light of the strongest visible hydrogen emission line.";
         this.image = this.img.nativeElement;
+        this.flex = this.content.nativeElement;
     }
 
     onScroll(args: ScrollEventData) {
-        console.log(args.scrollY);
+        console.log("args.scrollY: " + args.scrollY);
+        // console.log("this.flex.getActualSize(): " + this.flex.getActualSize());
+        console.log("this.flex.getMeasuredHeight(): " + this.flex.getMeasuredHeight());
 
-        this.image.animate({
-            translate: { x: 0, y: -args.scrollY }
-        })
+        if (args.scrollY <= this.flex.getMeasuredHeight()) {
+            this.image.animate({
+                translate: { x: 0, y: args.scrollY * 0.3 }
+            });
+        }
     }
 }
 // << content-scrollable-page-code
