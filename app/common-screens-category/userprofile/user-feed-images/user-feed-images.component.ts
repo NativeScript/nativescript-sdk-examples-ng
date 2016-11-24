25ef49f531928f95d1850d8f1f6e436a25aa85fc
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-11-24 18:40:53 +0200

    UI improvments user feed

--- app/common-screens-category/userprofile/user-feed-images/user-feed-images.component.ts
+++ app/common-screens-category/userprofile/user-feed-images/user-feed-images.component.ts
@@ -1,6 +1,6 @@
-import { Component, ChangeDetectionStrategy, OnInit, Input}  from "@angular/core";
-import {ObservableArray} from "data/observable-array";
-import {mockedDataArray} from "../mock-dataItems" 
+import { Component, ChangeDetectionStrategy, OnInit, Input } from "@angular/core";
+import { ObservableArray } from "data/observable-array";
+import { mockedDataArray } from "../mock-dataItems"
 
 // >> ext-listview-cards-code
 @Component({
@@ -8,14 +8,14 @@ import {mockedDataArray} from "../mock-dataItems"
     templateUrl: "common-screens-category/userprofile/user-feed-images/user-feed-images.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserFeedImagesExampleComponent  {
-    public countries:ObservableArray<any>;
+export class UserFeedImagesExampleComponent {
+    public countries: ObservableArray<any>;
 
-    constructor(){
-         this.countries = new ObservableArray(mockedDataArray)
+    constructor() {
+        this.countries = new ObservableArray(mockedDataArray)
     }
 
-    public followTap(){
+    public followTap() {
         console.log("follow button")
     }
 
--- app/common-screens-category/userprofile/user-settings-menu/user-settings-menu.component.ts
+++ app/common-screens-category/userprofile/user-settings-menu/user-settings-menu.component.ts
@@ -1,14 +1,14 @@
-import { Component, ChangeDetectionStrategy, OnInit, Input}  from "@angular/core";
-import {ObservableArray} from "data/observable-array";
-import {mockedDataArray} from "../mock-dataItems";
-import {ItemEventData} from "ui/list-view"
+import { Component, ChangeDetectionStrategy, OnInit, Input } from "@angular/core";
+import { ObservableArray } from "data/observable-array";
+import { mockedDataArray } from "../mock-dataItems";
+import { ItemEventData } from "ui/list-view"
 // >> userprofile-settings-menu-code
 @Component({
     selector: "user-settings-menu",
     templateUrl: "common-screens-category/userprofile/user-settings-menu/user-settings-menu.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserSettingsMenuExampleComponent  {
+export class UserSettingsMenuExampleComponent {
     public countries: ObservableArray<any>;
 
     ngOnInit() {
