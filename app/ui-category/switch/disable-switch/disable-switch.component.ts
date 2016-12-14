ebe8f8cff5266b595719f0279798507bb41a02af
sis0k0 <stanimira.vlaeva@gmail.com>
2016-12-14 14:32:16 +0200

    fix(ui-category): simplify disable-switch example

--- app/ui-category/switch/disable-switch/disable-switch.component.ts
+++ app/ui-category/switch/disable-switch/disable-switch.component.ts
@@ -1,4 +1,3 @@
-// >> disable-switch-code
 import { Component, ViewChild, ElementRef } from "@angular/core";
 import { Switch } from "ui/switch";
 
@@ -8,20 +7,4 @@ import { Switch } from "ui/switch";
     styleUrls: ["./../switch.style.css"],
 })
 export class DisableSwitchComponent {
-
-    @ViewChild("sw1") firstSwitch: ElementRef;
-    @ViewChild("sw2") secondSwitch: ElementRef;
-    @ViewChild("sw3") thirdSwitch: ElementRef;
-
-    public onTap() {
-        let firstsw: Switch = <Switch>this.firstSwitch.nativeElement;
-        let secondsw: Switch = <Switch>this.secondSwitch.nativeElement;
-        let thirdsw: Switch = <Switch>this.thirdSwitch.nativeElement;
-
-        firstsw.isEnabled = false;
-        secondsw.isEnabled = false;
-        thirdsw.isEnabled = false;
-    }
-
 }
-// << disable-switch-code
