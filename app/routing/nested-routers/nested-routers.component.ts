35738d2f5599d6c62de8181cf513195b1d28431b
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-01-04 16:39:17 +0200

    nested routers example

---
+++ app/routing/nested-routers/nested-routers.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    templateUrl: "./nested-routers.component.html"
+})
+export class NestedRoutersComponent {
+    constructor() { }
+}
---
+++ app/routing/nested-routers/sub-route-two.component.ts
@@ -0,0 +1,11 @@
+// >> 
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    templateUrl: "./sub-route-two.component.html"
+})
+export class SubRouteTwoComponent {
+    constructor() { }
+}
+// << 
