7f6bf0ecbc2323abbdd66448781f15300d11a866
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-03-22 23:24:58 +0200

    Support for Angular 4 (#167)
    * refactor: update to be compiant with Angular 4.0
    
    - import NativeScriptAnimationsModule
    - add HTMLMetaElement to references.d.ts
    - rename <template> to <ng-template>
    
    * fix(rxjs): import only Observable

--- references.d.ts
+++ references.d.ts
@@ -1,4 +1,25 @@
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
-
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+
+declare type Comment = any;
+declare type CloseEvent = any;
+declare type Document = any;
+declare type DocumentFragment = any;
+declare type Element = any;
+declare type History = any;
+declare type HTMLAnchorElement = any;
+declare type HTMLCollection = any;
+declare type HTMLDocument = any;
+declare type HTMLMetaElement = any;
+declare type HTMLElement = any;
+declare type HTMLInputElement = any;
+declare type HTMLScriptElement = any;
+declare type HTMLStyleElement = any;
+declare type KeyboardEvent = any;
+declare type Location = any;
+declare type MessageEvent = any;
+declare type MouseEvent = any;
+declare type Node = any;
+declare type NodeList = any;
+declare type Text = any;
+declare type WebSocket = any;
