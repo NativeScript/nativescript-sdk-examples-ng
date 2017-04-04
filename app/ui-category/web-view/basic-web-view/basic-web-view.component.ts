90d8fcb3913c97c61e404afb2dfa7e1bf68b8aec
vakrilov <alexander.vakrilov@telerik.com>
2017-04-04 18:04:08 +0300

    Update - basic wen view example

--- app/ui-category/web-view/basic-web-view/basic-web-view.component.ts
+++ app/ui-category/web-view/basic-web-view/basic-web-view.component.ts
@@ -1,5 +1,5 @@
 // >> setting-url-webview
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
 import { WebView, LoadEventData } from "ui/web-view";
 import { Page } from "ui/page";
 import { TextField } from "ui/text-field";
@@ -9,16 +9,14 @@ import { TextField } from "ui/text-field";
     templateUrl: "./basic-web-view.component.html",
     styleUrls: ["./style.css"]
 })
-export class BasicWebViewComponent implements OnInit {
+export class BasicWebViewComponent implements AfterViewInit {
+    public webViewSrc: string = "https://www.nativescript.org/";
 
-    public url = "https://www.google.com";
-    public webviewsrc = "https://www.google.com";
+    @ViewChild("myWebView") webViewRef: ElementRef;
+    @ViewChild("urlField") urlFieldRef: ElementRef;
 
-    constructor(private page: Page) {
-    }
-
-    ngOnInit() {
-        let webview: WebView = this.page.getViewById<WebView>("wv");
+    ngAfterViewInit() {
+        let webview: WebView = this.webViewRef.nativeElement;
 
         webview.on(WebView.loadFinishedEvent, function (args: LoadEventData) {
             let message;
@@ -31,35 +29,22 @@ export class BasicWebViewComponent implements OnInit {
         });
     }
 
-    loadPage() {
-        let textField: TextField = this.page.getViewById<TextField>("urlField");
-
-        if (this.url.substring(0, 4) === "http") {
-            this.webviewsrc = this.url;
-            textField.dismissSoftInput();
-        } else {
-            alert("Please, add `http://` or `https://` in front of the URL string");
-        }
-
-    }
-
     goBack() {
-        let webview: WebView = this.page.getViewById<WebView>("wv");
+        let webview: WebView = this.webViewRef.nativeElement;
         if (webview.canGoBack) {
             webview.goBack();
         }
     }
 
     submit(args: string) {
-        let textField: TextField = this.page.getViewById<TextField>("urlField");
+        let textField: TextField = this.urlFieldRef.nativeElement;
 
         if (args.substring(0, 4) === "http") {
-            this.webviewsrc = args;
+            this.webViewSrc = args;
             textField.dismissSoftInput();
         } else {
             alert("Please, add `http://` or `https://` in front of the URL string");
         }
     }
-
 }
 // << setting-url-webview
