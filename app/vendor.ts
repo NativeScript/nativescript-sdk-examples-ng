eca8e099332c5dab27897410886df7b3e68a6f60
Nick Iliev <nikolay.iliev@telerik.com>
2017-12-19 19:31:13 +0200

    refactor: update Angular-5 and WebPack dependencies (#245)
    * refactor: Update Angular-5 and WebPack dependencies
    Updated all dependencies and devDependencies to comply with the next branches of nativescript-angular and nativescript-dev-webpack
    
    * refactor: added tilde for app.css + updated vendor.ts file
    
    * chore: bump version of runtimes, rxjs and tsc
    
    * refactor: update tsconfig.json file

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,3 +1,10 @@
+// Snapshot the ~/app.css and the theme
+const application = require("application");
+require("ui/styling/style-scope");
+const appCssContext = require.context("~/", false, /^\.\/app\.(css|scss|less|sass)$/);
+global.registerWebpackModules(appCssContext);
+application.loadAppCss();
+
 require("./vendor-platform");
 
 require("reflect-metadata");
@@ -9,6 +16,5 @@ require("@angular/http");
 require("@angular/router");
 
 require("nativescript-angular/platform-static");
-require("nativescript-angular/router");
 require("nativescript-angular/forms");
-
+require("nativescript-angular/router");
