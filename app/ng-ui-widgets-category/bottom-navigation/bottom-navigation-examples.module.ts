c8b75fb611abd76ce0770d8edd9f03caabca94dc
NickIliev <Nikolay.Iliev@telerik.com>
2019-07-08 15:05:25 +0300

    docs: BottomNavigation  example

--- app/ng-ui-widgets-category/bottom-navigation/bottom-navigation-examples.module.ts
+++ app/ng-ui-widgets-category/bottom-navigation/bottom-navigation-examples.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { BottomNavigationExamplesComponent } from "./button-examples.component";
+import { BottomNavigationExamplesComponent } from "./bottom-navigation-examples.component";
 import { UsageComponent } from "./usage/usage.component";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { TitleAndNavButtonModule } from "../../directives/title-and-nav-button.module";
