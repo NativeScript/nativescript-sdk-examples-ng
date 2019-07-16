33aa1edf7b54f1bc5fc43cccec9582db50b634d8
NickIliev <Nikolay.Iliev@telerik.com>
2019-07-16 13:06:05 +0300

    docs: docker build tslint rule

--- app/ng-ui-widgets-category/tabs/tips-and-tricks/tips-and-tricks.component.ts
+++ app/ng-ui-widgets-category/tabs/tips-and-tricks/tips-and-tricks.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
     moduleId: module.id,
     templateUrl: "./tips-and-tricks.component.html",
-    styleUrls:["./tips-and-tricks.component.css"]
+    styleUrls: ["./tips-and-tricks.component.css"]
 })
 export class TabsTipsAndTricksComponent { }
